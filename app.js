ba68729a1aa355fb8e8e39b13434c35bb7a4b09d
ugtakhbayar surakhbayar <ugtakhbayars@gmail.com>
2023-03-11 10:29:19 +0800

    finish user controller with mysql

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-console.log("OB");
+// console.log("DESTRUCT");
 
-const body = { name: "naraa", age: 23 };
+// const body = { name: "naraa", age: 23 };
 
-const keys = Object.keys(body);
-const values = Object.values({ name: "naraa" });
-const ob = Object.entries(body);
+// const keys = Object.keys(body);
+// const values = Object.values({ name: "naraa" });
+// const ob = Object.entries(body);
 
-console.log(keys);
-console.log(values);
+// console.log(keys);
+// console.log(values);
 
-const a = keys.map((key) => `${key}="${body[key]}"`);
-const b = a.join();
-console.log(a);
+// const a = keys.map((key) => `${key}="${body[key]}"`);
+// const b = a.join();
+// console.log(a);
 
-// UPDATE tableName SET b WHERE key1 = value1 AND key2 = value2 AND key3 = value3;
+const ob = ["name='Naraa'", "age='23'"];
 
-const convert = async (emp_no, updatedData) => {
-  console.time("2023:03:03");
-  let [result] = "";
-  for (let i = 0; i < Object.keys(updatedData).length; i++) {
-    result = await pool.query(
-      `UPDATE employees SET ${Object.keys(updatedData)[i]} ='${
-        Object.values(updatedData)[i]
-      }'  WHERE emp_no = ${emp_no}`
-    );
-  }
-  console.time("2023:03:03");
-  return result;
-};
-
-const ss = convert(1, {});
-
-console.log("Convert:");
+console.log(ob);
+console.log(ob.join());
+console.log(ob.join(""));
+console.log(ob.join(","));
--- controllers/users.js
+++ controllers/users.js
@@ -1,43 +1,36 @@
-const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
 const connection = require("../config/db");
-
-const filePath = "./data/users.json";
+const { convertQueryStr } = require("../utils/convertQuery");
 
 const getAllUsers = (req, res) => {
-  fs.readFile(filePath, "utf-8", (err, data) => {
+  const query = `SELECT id, name, email, phone_number, profileImg FROM user`;
+  connection.query(query, (err, result) => {
     if (err) {
-      console.log("Файл уншихад алдаа гарлаа. !!!");
+      res.status(400).json({ error: err.message });
       return;
     }
-    const parsedData = JSON.parse(data);
-    res.status(201).json({ users: parsedData.datas });
+    console.log(result);
+    res.status(200).json({ data: result });
   });
 };
 
 const getUser = (req, res) => {
   const { id } = req.params;
-  const query = `SELECT * FROM user WHERE id=?`;
+  const query = `SELECT id, name, email, phone_number, profileImg FROM user WHERE id=?`;
   connection.query(query, [id], (err, result) => {
     if (err) {
       res.status(400).json({ error: err.message });
       return;
     }
-    res.status(200).json({ data: result[0] });
+    res.status(200).json({ data: result });
   });
 };
 
 const createUser = (req, res) => {
   const { name, email, password, phoneNumber } = req.body;
+
   const salted = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salted);
-
-  // console.log(name);
-  // console.log(email);
-  // console.log(password);
-  // console.log(phoneNumber);
-  // res.status(201).json({ message: "OK" });
 
   const query =
     "INSERT INTO user (id, name,email,password, phone_number, profileImg) VALUES(null, ?, ?, ?, ?, ?)";
@@ -58,27 +51,33 @@ const createUser = (req, res) => {
 
 const updateUser = (req, res) => {
   const { id } = req.params;
-  const { name } = req.body;
-  const data = fs.readFileSync(filePath, "utf-8");
-  const parsedData = JSON.parse(data);
-  const findIndex = parsedData.datas.findIndex((el) => el.id === id);
-  parsedData.users[findIndex].name = name;
-  fs.writeFileSync(filePath, JSON.stringify(parsedData));
-  res
-    .status(201)
-    .json({ message: "Шинэ хэрэглэгчийн өгөгдөл амжилттай солигдлоо." });
+  const updateData = convertQueryStr(req.body);
+  const query = `UPDATE user SET ${updateData} WHERE id=?`;
+  connection.query(query, [id], (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err.message });
+      return;
+    }
+
+    res.status(201).json({
+      message: `${id} тэй хэрэглэгчийн өгөгдөл амжилттай шинэчлэгдлээ.`,
+    });
+  });
 };
 
 const deleteUser = (req, res) => {
   const { id } = req.params;
-  const data = fs.readFileSync(filePath, "utf-8");
-  const parsedData = JSON.parse(data);
-  const findIndex = parsedData.datas.findIndex((el) => el.id === id);
-  parsedData.datas.splice(findIndex, 1);
-  fs.writeFileSync(filePath, JSON.stringify(parsedData));
-  res
-    .status(201)
-    .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
+
+  const query = `DELETE user WHERE id=?`;
+  connection.query(query, [id], (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err.message });
+      return;
+    }
+    res
+      .status(201)
+      .json({ message: `${id} тай хэрэглэгч амжилттай устгагдлаа.` });
+  });
 };
 
 module.exports = { getAllUsers, getUser, createUser, updateUser, deleteUser };
